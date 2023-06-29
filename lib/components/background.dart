89ab79b4ec056078bc69ca6c32429e5ec9ce1794
Erick Zanardo <erickzanardoo@gmail.com>
2023-06-29 20:41:56 -0300

    fix: parallax quality

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -8,17 +8,53 @@ import 'package:space_shooter_workshop/game.dart';
 class Background extends ParallaxComponent<SpaceShooterGame> {
   @override
   FutureOr<void> onLoad() async {
-    parallax = await game.loadParallax(
-      [
-        ParallaxImageData('background_nebula.png'),
-        ParallaxImageData('background_stars_1.png'),
-        ParallaxImageData('background_stars_2.png'),
-        ParallaxImageData('background_stars_3.png'),
-      ],
-      baseVelocity: Vector2(0, -1),
-      velocityMultiplierDelta: Vector2(0, 4.0),
-      repeat: ImageRepeat.repeat,
-    );
+    //parallax = await game.loadParallax(
+    //  [
+    //    ParallaxImageData('background_nebula.png'),
+    //    ParallaxImageData('background_stars_1.png'),
+    //    ParallaxImageData('background_stars_2.png'),
+    //    ParallaxImageData('background_stars_3.png'),
+    //  ],
+    //  baseVelocity: Vector2(0, -1),
+    //  velocityMultiplierDelta: Vector2(0, 4.0),
+    //  repeat: ImageRepeat.repeat,
+    //);
+
+    // TODO(erickzanardo): Use the above API when
+    // is merged: https://github.com/flame-engine/flame/pull/2596
+    final parallaxImages = await Future.wait([
+      game.loadParallaxImage(
+        'background_nebula.png',
+        filterQuality: FilterQuality.none,
+        repeat: ImageRepeat.repeat,
+      ),
+      game.loadParallaxImage(
+        'background_stars_1.png',
+        filterQuality: FilterQuality.none,
+        repeat: ImageRepeat.repeat,
+      ),
+      game.loadParallaxImage(
+        'background_stars_2.png',
+        filterQuality: FilterQuality.none,
+        repeat: ImageRepeat.repeat,
+      ),
+      game.loadParallaxImage(
+        'background_stars_3.png',
+        filterQuality: FilterQuality.none,
+        repeat: ImageRepeat.repeat,
+      ),
+    ]);
+
+    final layers = parallaxImages
+        .map(
+          (e) => ParallaxLayer(
+            e,
+            velocityMultiplier: Vector2(0, 4.0),
+          ),
+        )
+        .toList();
+
+    parallax = Parallax(layers, baseVelocity: Vector2(0, -1));
 
     size = gameRef.size.clone();
   }
