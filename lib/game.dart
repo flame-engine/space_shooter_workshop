ac91ed3d872a91e22836ca7fe7e67123a947850c
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2025-10-25 15:25:25 +0200

    Temporary branch

--- lib/game.dart
+++ lib/game.dart
@@ -1,11 +1,20 @@
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:space_shooter_workshop/components/components.dart';
 
-class SpaceShooterGame extends FlameGame {
-  SpaceShooterGame()
-      : super(
-          children: [
-            Player(),
-          ],
-        );
+class SpaceShooterGame extends FlameGame
+    with MouseMovementDetector, TapCallbacks {
+  SpaceShooterGame() : player = Player() {
+    add(player);
+  }
+
+  final Player player;
+
+  @override
+  void onMouseMove(PointerHoverInfo event) {
+    player.position = event.eventPosition.global;
+  }
+
+  @override
+  void onTapDown(TapDownEvent event) {}
 }
